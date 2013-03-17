17519f5fa570b7e1b18b7b9b0e3fc052e8dc7a53
Herover <leon@Jumpy.(none)>
2013-03-17 20:34:12 +0100

    initial code

--- remote/config.php
+++ remote/config.php
@@ -1,9 +1,9 @@
 <?php
-$LOGPATH = "/var/www/remote_log";
-$INCPATH = "/var/www/remote_inc";
+$LOGPATH = "/var/www/hl2/remote_log";
+$INCPATH = "/var/www/hl2/remote_inc";
 
 $MYSQL_USER = "USER";
 $MYSQL_PASS = "PASS";
 
-$ORIGIN = "http://remote.lolbrothers.com";
+$ORIGIN = "http://remote.lolbrothers.com/hl2";
 ?> 
